ecd1f8c44df61395590e5aa2b126714faac5fcdf
tckastanek <tckastanek@gmail.com>
2017-08-01 11:25:36 -0400

    Implemented get, remove and static methods.

--- binary-trees-thomas-william/app.js
+++ binary-trees-thomas-william/app.js
@@ -29,4 +29,84 @@ class BinarySearchTree{
       }
     }
   }
+
+  get(key) {
+    if (this.key === key) {
+      return this.value;
+    }
+    else if (key < this.key && this.left) {
+      this.left.get(key);
+    }
+    else if (key > this.key && this.right) {
+      return this.right.get(key);
+    }
+    else {
+      throw new Error('Key Error');
+    }
+  }
+
+  remove(key) {
+    if (this.key === key) {
+      if (this.left && this.right) {
+        const successor = this.right._findMin();
+        // successor = this.left._findMax();
+        this.key = successor.key;
+        this.value = successor.value;
+        successor.remove(successor.key);
+      }
+      else if (this.left) {
+        this._replaceWith(this.left);
+      }
+      else if (this.right) {
+        this._replaceWith(this.right);
+      }
+      else {
+        this._replaceWith(null);
+      }
+    }
+    else if (key < this.key && this.left) {
+      this.left.remove(key);
+    }
+    else if (key > this.key && this.right) {
+      this.right.remove(key);
+    }
+    else {
+      throw new Error('Key Error');
+    }
+  }
+
+  _replaceWith(node) {
+    if (this.parent) {
+      if (this === this.parent.left) {
+        this.parent.left = node;
+      }
+      else if (this === this.parent.right) {
+        this.parent.right = node;
+      }
+      if (node) {
+        node.parent = this.parent;
+      }
+    }
+    else {
+      if (node) {
+        this.key = node.key;
+        this.value = node.value;
+        this.left = node.left;
+        this.right = node.right;
+      }
+      else {
+        this.key = null;
+        this.value = null;
+        this.left = null;
+        this.right = null;
+      }
+    }
+  }
+
+  _findMin() {
+    if (!this.left) {
+      return this;
+    }
+    return this.left._findMin();
+  }
 }

6ed20e4b3176ffc57778fe8a8f134d93b2a1956b
williamtwobit <william@twobit.cc>
2017-08-01 11:07:42 -0400

    implement insert method

--- binary-trees-thomas-william/app.js
+++ binary-trees-thomas-william/app.js
@@ -1,1 +1,32 @@
-console.log('hello world');
+class BinarySearchTree{
+  constructor(key=null,value=null,parent=null){
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(key, value){
+    if(this.key === null){
+      this.key = key;
+      this.value = value;
+    }
+    else if(key < this.key){
+      if(this.left === null){
+        this.left = new BinarySearchTree(key, value, this);
+      }
+      else{
+        this.left.insert(key,value);
+      }
+    }
+    else{
+      if(this.right === null){
+        this.right = new BinarySearchTree(key, value, this);
+      }
+      else{
+        this.right.insert(key,value);
+      }
+    }
+  }
+}
